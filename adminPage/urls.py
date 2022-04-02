8b226afb8f961d3f3bb0ff284ea31574ab6140c7
Begzod <97680127+Begzod021@users.noreply.github.com>
2022-04-02 17:56:12 +0500

    AdminPage function

--- adminPage/urls.py
+++ adminPage/urls.py
@@ -6,6 +6,11 @@ from .views import *
 
 urlpatterns = [
     path('', user_login, name='login'),
+    path('add-product/', add_product, name='add_product'),
+    path('product/', admin_product, name='admin_product'),
+    path('brand-edit/<slug:slug>/', editbrand, name='editbrand'),
+    path('product-edit/<slug:slug>/', editproduct, name='editproduct'),
+    path('delete/<int:pk>/', delete_product, name='delete'),
     path('add-admin/', addAdmin, name='add-admin'),
     path('logout/', user_logout, name='logout'),
     path('profile/<str:username>/', profile, name='profile'),
